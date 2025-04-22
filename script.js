28e54d4273acbd512c3c208b7878039bfc09d2fe
aaerial <aerialabella0@gmail.com>
2025-04-23 04:04:50 +0800

    Finished the tourist spot section. Off to the food section

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+let places = [
+    { name: "Colosseum - Rome's Timeless Arena", about: "Step into the heart of ancient Rome with a visit to the Colosseum, the largest amphitheater ever built. Once home to gladiator battles and grand spectacles, it’s a powerful reminder of the empire’s architectural brilliance and dramatic history." },
+    { name: "Venice Grand Canal – A City on Water", about: "Glide through the romantic waterways of Venice on a gondola ride along the Grand Canal. Lined with Renaissance palaces and bustling bridges, it’s a living postcard of Italy’s unique charm and elegance." },
+    { name: "Leaning Tower of Pisa – An Icon of Imperfection", about: "A marvel of medieval engineering gone slightly sideways, the Leaning Tower of Pisa is world-famous for its tilt. Climb its spiraling staircase for panoramic views and a photo-op you can’t leave Italy without." },
+    { name: "Amalfi Coast – Italy’s Coastal Paradise", about: "With its colorful cliffside villages, crystal-clear waters, and sun-soaked beaches, the Amalfi Coast is a dream destination. Whether you're savoring local seafood or hiking scenic trails, it’s pure Mediterranean magic." },
+    { name: "Florence Cathedral – The Dome of the Renaissance", about: "Dominating the skyline of Florence, the Cathedral of Santa Maria del Fiore (Duomo) is a masterpiece of Gothic and Renaissance architecture. Its iconic red dome and detailed façade are as awe-inspiring up close as they are from afar." }
+];
+// console.log(places[1].name);
+
 // IMAGE SLIDER   
+
+const pisaH = document.querySelector("#place-about h1");
+const pisaP = document.querySelector("#place-about p");
 
 const slides = document.querySelectorAll("#slides img");   //gets all img elements inside the element with id="slides".
 let slideIndex = 0;
@@ -28,7 +40,18 @@ function showSlide(index) { // handles showing the slide based on index:
         slides.classList.remove("displaySlide"); //all image element is removed to the .displaySlide for it to not show
     });
     slides[slideIndex].classList.add("displaySlide");  //the image in the current index will be shown since display = none;
-    document.getElementById("curr-slide-no").textContent = slideIndex+1;
+
+    document.getElementById("curr-slide-no").textContent = slideIndex + 1;
+    document.getElementById("name-place").textContent = places[slideIndex].name;
+    document.getElementById("place-desc").textContent = places[slideIndex].about;
+
+    if (slideIndex == 2) {
+        pisaH.classList.add("pisa-Exclusive");
+        pisaP.classList.add("pisa-Exclusive");
+    } else {
+        pisaH.classList.remove("pisa-Exclusive");
+        pisaP.classList.remove("pisa-Exclusive");
+    }
 }
 
 function prevSlide() {
